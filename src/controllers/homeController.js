4b4224e005f7783eca4137cecb071a56df4f50fe
tinhduytinh <tinhtrinh54@gmail.com>
2023-09-18 15:48:22 +0700

    config app

---
+++ src/controllers/homeController.js
@@ -0,0 +1,24 @@
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+        data: JSON.stringify(data)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let getAboutPage = (req, res) => {
+  return res.render("test/about.ejs");
+};
+// Object: {
+//     key: '',
+//     value: ''
+// }
+module.exports = {
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+};
