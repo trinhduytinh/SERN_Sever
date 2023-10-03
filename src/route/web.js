b89c5c9295042397af5a25481606d89b1487cce8
tinhduytinh <tinhtrinh54@gmail.com>
2023-10-03 08:41:33 +0700

    add api login

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
-
+import userController from "../controllers/userController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -8,6 +8,19 @@ let initWebRoutes = (app) => {
 
   router.get("/about", homeController.getAboutPage);
 
+  router.get("/crud", homeController.getCRUD);
+
+  router.post("/post-crud", homeController.postCRUD);
+
+  router.get("/get-crud", homeController.displayGetCRUD);
+
+  router.get("/edit-crud", homeController.getEditCRUD);
+
+  router.post('/put-crud', homeController.putCRUD);
+
+  router.get('/delete-crud', homeController.deleteCRUD);
+
+  router.post('/api/login', userController.handleLogin);
   return app.use("/", router);
 };
 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB from "./config/connectDB"
+import connectDB from "./config/connectDB";
+import cors from "cors";
+
 require("dotenv").config();
 
 let app = express();
-
+app.use(cors({ credentials: true ,origin: true }));
 //config app
 
 app.use(bodyParser.json());
@@ -24,5 +26,3 @@ app.listen(port, () => {
   //callback
   console.log("Backend Nodejs is runing on the port: " + port);
 });
-
-
